8a18042a2342ff654aa1f0c9e2b2e77c10a2e93f
Fuuuzer <86896140+Fuuuzer@users.noreply.github.com>
2022-06-27 19:42:47 -0300

    Este arquivo é o principal do nosso sistema

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $nota1 = 10;
+    $nota2 = 2;
+    $media = ($nota1 + $nota2)/2;
+
+    if ($media>=6){
+        echo "o aluno foi aprovado";
+    } else {
+        echo "O Aluno foi reprovado";
+    }
+?>
